1b9682ad75b1b0bb7c43c77b212917200eade5af
souad-outh <southarout@gmail.com>
2025-05-10 14:58:34 +0100

    feat(middleware): application des politiques CSP via middleware

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        
+        // Add Content Security Policy headers
+        $response->headers->set('Content-Security-Policy', $this->getPolicy());
+        
+        return $response;
+    }
+    
+    /**
+     * Get the Content Security Policy.
+     *
+     * @return string
+     */
+    protected function getPolicy(): string
+    {
+        return implode('; ', [
+            "default-src 'self'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tailwindcss.com https://cdn.jsdelivr.net",
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.tailwindcss.com https://cdn.jsdelivr.net",
+            "img-src 'self' data: https:",
+            "font-src 'self' https://fonts.gstatic.com",
+            "connect-src 'self'",
+            "media-src 'self'",
+            "object-src 'none'",
+            "frame-src 'self'",
+            "base-uri 'self'",
+            "form-action 'self'",
+            "frame-ancestors 'self'",
+        ]);
+    }
+}
