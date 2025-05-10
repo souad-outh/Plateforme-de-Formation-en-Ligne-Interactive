a9b1d08fe014ce724806eb7da068ce71739e83b6
souad-outh <southarout@gmail.com>
2025-05-10 14:58:22 +0100

    feat(middleware): middleware pour vérifier la double authentification

---
+++ app/Http/Middleware/TwoFactorChallenge.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class TwoFactorChallenge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if ($user && $user->hasTwoFactorEnabled() && !session('auth.two_factor.verified')) {
+            return redirect()->route('two-factor.challenge');
+        }
+
+        return $next($request);
+    }
+}
